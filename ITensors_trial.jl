44607de31f2fbeaf751d2e019acd81e0447236bd
TheoVereka <131789798+TheoVereka@users.noreply.github.com>
2024-10-15 15:01:01 +0200

    a

--- ITensors_trial.jl
+++ ITensors_trial.jl
@@ -1,4 +1,12 @@
-##
-using ITensors; # the tutorial of C++ is much better than the online version of Julia
-println("Hello ITensor!")
+using ITensors
+let
+  i = Index(10)           # index of dimension 10
+  j = Index(20)           # index of dimension 20
+  M = random_itensor(i,j)  # random matrix, indices i,j
+  U,S,V = svd(M,i)        # compute SVD with i as row index
+  @show M ≈ U*S*V         # = true
 
+  nothing
+end
+
+println(M)
