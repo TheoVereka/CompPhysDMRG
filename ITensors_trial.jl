3fbec708afa2a2b64c4b9578b711bf5b4837bdf0
TheoVereka <18752091160@163.com>
2024-10-15 15:19:56 +0200

    now print the Signular Value tensor

--- ITensors_trial.jl
+++ ITensors_trial.jl
@@ -1,11 +1,16 @@
+
+##
 using ITensors;
+
+##
 let
   i = Index(10)           # index of dimension 10
   j = Index(20)           # index of dimension 20
   M = random_itensor(i,j)  # random matrix, indices i,j
   U,S,V = svd(M,i)        # compute SVD with i as row index
-  @show M ≈ U*S*V         # = true
-
+  @show M ≈ U*S*V         # OUTPUT " ... = true "
+  println("\n")           # may slow down
+  @show S
   nothing
 end
 
